9a255d3d30ee6a451ec8f4a7dfdb13524413959f
David <davidriosbenet@gmail.com>
2021-06-23 09:10:45 +0200

    feat: add control validation

--- src/app/modules/shared/directives/controlError/controlError.directive.ts
+++ src/app/modules/shared/directives/controlError/controlError.directive.ts
@@ -6,16 +6,23 @@ import {
   Self,
   ViewContainerRef,
 } from '@angular/core';
-import { NgControl } from '@angular/forms';
-import { filter, map } from 'rxjs/operators';
-import { Overlay } from '@angular/cdk/overlay';
+import { NgControl, ValidationErrors } from '@angular/forms';
+import { filter, map, tap } from 'rxjs/operators';
+import {
+  FlexibleConnectedPositionStrategy,
+  Overlay,
+  OverlayRef,
+} from '@angular/cdk/overlay';
 
 import { ComponentPortal } from '@angular/cdk/portal';
 import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
+import { VALIDATORS_MESSAGES } from 'src/app/validators/messages';
 
 @Directive({ selector: '[controlError]' })
 export class ControlErrorDirective implements AfterViewInit {
-  componentRef: ComponentRef<ErrorMessageComponent>;
+  private componentRef: ComponentRef<ErrorMessageComponent>;
+  private overlayRef: OverlayRef;
+
   constructor(
     @Self() private control: NgControl,
     private overlay: Overlay,
@@ -24,20 +31,11 @@ export class ControlErrorDirective implements AfterViewInit {
 
   ngAfterViewInit() {
     this.subscribeToChanges();
+    this.createOverlay();
   }
 
-  private subscribeToChanges(): void {
-    this.control.valueChanges
-      .pipe(
-        map(() => this.control.errors),
-        filter((errors: { [key: string]: any }) => !!errors)
-      )
-      .subscribe((res: { [key: string]: any }) => this.setError());
-  }
-
-  private setError(): void {
-    const ref = new ComponentPortal(ErrorMessageComponent);
-    const positionStrategy = this.overlay
+  get position(): FlexibleConnectedPositionStrategy {
+    return this.overlay
       .position()
       .flexibleConnectedTo(this.elementRef)
       .withPositions([
@@ -49,13 +47,47 @@ export class ControlErrorDirective implements AfterViewInit {
           offsetY: 35,
         },
       ]);
+  }
 
-    const overlayRef = this.overlay.create({
-      positionStrategy,
+  private subscribeToChanges(): void {
+    this.control.valueChanges
+      .pipe(
+        map(() => this.control.errors),
+        tap((errors: ValidationErrors) => this.checkError(errors)),
+        filter((errors: { [key: string]: any }) => !!errors),
+        tap(() => this.createErrorComponent())
+      )
+      .subscribe((errors: { [key: string]: any }) => this.setError(errors));
+  }
+
+  private checkError(errors: ValidationErrors): void {
+    if (!errors && this.componentRef) {
+      this.overlayRef.detach();
+      this.componentRef = null;
+    }
+  }
+
+  private createOverlay() {
+    this.overlayRef = this.overlay.create({
+      positionStrategy: this.position,
       panelClass: ['panel--error'],
     });
+  }
 
-    this.componentRef = overlayRef.attach(ref);
-    this.componentRef.instance.msg = 'Hola2';
+  private createErrorComponent(): void {
+    if (this.componentRef) {
+      return;
+    }
+
+    const ref = new ComponentPortal(ErrorMessageComponent);
+
+    this.componentRef = this.overlayRef.attach(ref);
+  }
+
+  private setError(errors: { [key: string]: any }): void {
+    const [firstError] = Object.entries(errors);
+
+    const error = VALIDATORS_MESSAGES[firstError[0]](firstError[1]);
+    this.componentRef.instance.msg = error;
   }
 }
