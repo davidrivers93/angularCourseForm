1bcc3497b8f6c4acd581242f435886850f8c2bf0
David <davidriosbenet@gmail.com>
2021-06-27 11:39:59 +0200

    feat: add coverage script

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
+  coverageReporters: ["text", "text-summary", "html"],
 };
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Profile } from '../models/profile.model';
+import { Profile } from '../modules/core/models/profile.model';
 
 @Injectable()
 export class ProfileService {
