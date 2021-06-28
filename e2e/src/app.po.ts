8bd06f6cc019732a311e541443176a2867f8139c
David <davidriosbenet@gmail.com>
2021-06-28 10:04:45 +0200

    Feat: add e2e test example

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
@@ -7,5 +7,32 @@ export class AppPage {
 
   async getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText();
+  }
+
+  async getFormItemByFormControlName(
+    controlName: string
+  ): Promise<ElementFinder> {
+    by.addLocator(
+      'formControlName',
+      function (value, opt_parentElement, opt_rootSelector) {
+        var using = opt_parentElement || document;
+
+        return using.querySelectorAll('[formControlName="' + value + '"]');
+      }
+    );
+
+    return element(by.formControlName(controlName));
+  }
+
+  async getItemByCss(css: string): Promise<ElementFinder> {
+    return element(by.css(css));
+  }
+
+  async getButtonByText(text: string) {
+    return element(by.buttonText(text));
+  }
+
+  async getItemByPlaceholder(placeholder: string) {
+    return element(by.css(`[placeholder = ${placeholder}]`));
   }
 }
